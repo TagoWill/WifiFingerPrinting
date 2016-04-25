7d00094add295b522853e8ecf82e50ac32e71e7c
dbastos10 <dani_b10_7@hotmail.com>
2016-04-25 01:57:38 +0100

    Scan Wi-Fi done but test isn't working on emulator. Analysis Activity started.

---
+++ app/src/main/java/com/project2/su/wififingerprinting/AnalysisActivity.java
@@ -0,0 +1,14 @@
+package com.project2.su.wififingerprinting;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AnalysisActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis);
+
+    }
+}
--- app/src/main/java/com/project2/su/wififingerprinting/MainActivity.java
+++ app/src/main/java/com/project2/su/wififingerprinting/MainActivity.java
@@ -1,13 +1,76 @@
 package com.project2.su.wififingerprinting;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    WifiManager wifi;
+    String wifis[];
+    WifiScanReceiver wifiReciever;
+    Button buttonScan, buttonAnalysis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv=(ListView)findViewById(R.id.listView);
+
+        wifi=(WifiManager)getSystemService(Context.WIFI_SERVICE);
+        wifiReciever = new WifiScanReceiver();
+
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Scanning....", Toast.LENGTH_SHORT).show();
+                wifi.startScan();
+            }
+        });
+
+        buttonAnalysis = (Button) findViewById(R.id.buttonAnalysis);
+        buttonAnalysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Analysing....", Toast.LENGTH_SHORT).show();
+                Intent analyse = new Intent(MainActivity.this, AnalysisActivity.class);
+                MainActivity.this.startActivity(analyse);
+            }
+        });
+    }
+
+    protected void onPause() {
+        unregisterReceiver(wifiReciever);
+        super.onPause();
+    }
+
+    protected void onResume() {
+        registerReceiver(wifiReciever, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        super.onResume();
+    }
+
+    private class WifiScanReceiver extends BroadcastReceiver{
+        public void onReceive(Context c, Intent intent) {
+            List<ScanResult> wifiScanList = wifi.getScanResults();
+            wifis = new String[wifiScanList.size()];
+
+            for(int i = 0; i < wifiScanList.size(); i++){
+                wifis[i] = ((wifiScanList.get(i)).toString());
+            }
+            lv.setAdapter(new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1,wifis));
+        }
     }
 }
